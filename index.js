99daddfef9fa33660f63df8cc37f3a1eaa4d294d
Dhruv <work.dhruvbhardwaj1s@gmail.com>
2026-03-21 18:24:19 +0530

    final

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+// console.log("Hello World!");
+// console.log("I like pizza!");
 
+// window.alert("This is an alert");
+// window.alert("I like pizza!");
+
+document.getElementById("myH1").textContent = "Hello World!";
+document.getElementById("myP").textContent = "This is a paragraph.";
