5615c44cf1db4f5cdf52fea39cff073e0167cb1b
Dhruv <work.dhruvbhardwaj1s@gmail.com>
2026-03-22 21:43:30 +0530

    Again

--- index.js
+++ index.js
@@ -4,5 +4,37 @@
 // window.alert("This is an alert");
 // window.alert("I like pizza!");
 
-document.getElementById("myH1").textContent = "Hello World!";
-document.getElementById("myP").textContent = "This is a paragraph.";
+// document.getElementById("myH1").textContent = "Hello World!";
+// document.getElementById("myP").textContent = "This is a paragraph.";
+
+// variable = A container that stores a value.
+//            Behave as if it were the value it contains.
+
+// 1. Declaration let x;
+// 2. Assignment x = 100;
+
+// let age = 21;
+// let gpa = 9;
+
+// console.log(`You are ${age} years old.`);
+// console.log(`Your gpa is: ${gpa}`);
+
+// let firstName = "Dhruv";
+// console.log(`Your name is ${firstName}`);
+// console.log(typeof firstName);
+
+// let online = true;
+// console.log(`Bro is online: ${online}`);
+
+// Exercise:
+
+// let fullName = "Dhruv Bhardwaj";
+// let age = 21;
+// let isStudent = true;
+
+// document.getElementById("p1").textContent = `My name is ${fullName}`;
+// document.getElementById("p2").textContent = `I am ${age}`;
+// document.getElementById("p3").textContent = `Am I a student? ${isStudent}`;
+
+// arithmetic operators: +, -, *, /, %, **
+// operator precedence: 1.parenthese 2.exponents 3.multiplication/division/module 4.addition/subtraction
